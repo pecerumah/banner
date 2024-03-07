ef6ed458e38fbded73558eaacdf6969e24d3dd99
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-03-07 23:22:44 +0700

    Update costom-botton-shoope.js

--- costom-botton-shoope.js
+++ costom-botton-shoope.js
@@ -1,1 +1,9 @@
-(()=>{const costomAds="https://shope.ee/2fZYAwJvcW";let elTarget=document.querySelector(".sg-button__text");if(elTarget){elTarget.addEventListener("click",()=>{window.open(costomAds,"_blank");});};})();
+(() => {
+    const costomAds = "https://shope.ee/2fZYAwJvcW";
+    let elTarget = document.querySelector(".sg-button__text");
+    if (elTarget) {
+        elTarget.addEventListener("click", () => {
+            window.open(costomAds, "_blank");
+        });
+    };
+})();
