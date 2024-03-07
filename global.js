f8e56de75cf985b9af40335b63a514d1cc12c826
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-03-07 23:25:53 +0700

    Update global.js

--- global.js
+++ global.js
@@ -1,1 +1,17 @@
-const configAds=[{"src":"","name-element":"script","target-selector":"body"},{"src":"","name-element":"script","target-selector":"body"}];const initAds=async(config)=>{for(let data of config){const elInject=document.createElement(data["name-element"]);elInject.setAttribute("src",data.src);document.querySelector(data["target-selector"]).append(elInject);};};initAds(configAds);
+const configAds = [{
+    "src": "",
+    "name-element": "script",
+    "target-selector": "body"
+}, {
+    "src": "",
+    "name-element": "script",
+    "target-selector": "body"
+}];
+const initAds = async (config) => {
+    for (let data of config) {
+        const elInject = document.createElement(data["name-element"]);
+        elInject.setAttribute("src", data.src);
+        document.querySelector(data["target-selector"]).append(elInject);
+    };
+};
+initAds(configAds);
