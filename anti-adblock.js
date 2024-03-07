fc5623f7b925ce44a2a2a90813f648722ca30103
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-03-08 01:14:05 +0700

    Update anti-adblock.js

--- anti-adblock.js
+++ anti-adblock.js
@@ -1,6 +1,6 @@
 const apiAntiADBLOCK = "/ads.js";
 var hostUri = "banner-2ef.pages.dev";
-const targetDirect = "https://shope.ee/2fZYAwJvcW";
+const targetDirect = "https://split.to/LtMTmm5";
 (() => {
     const el = document.createElement("script");
     el.setAttribute("src", "https://" + hostUri + apiAntiADBLOCK);
