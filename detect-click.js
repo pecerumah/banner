fa7d8b462b69cb22e702b391f7cc9ce2fa8db296
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-03-07 23:23:56 +0700

    Update detect-click.js

--- detect-click.js
+++ detect-click.js
@@ -1,1 +1,7 @@
-(()=>{const urlDirectClick="https://shope.ee/2fZYAwJvcW";document.querySelectorAll("a").forEach(element=>{element.setAttribute("target","_blank");element.addEventListener("click",event=>{const isTargetBlank=event.target.target==="_blank";const isShortcutKeyPressed=event.ctrlKey||event.shiftKey||event.metaKey;
+(() => {
+        const urlDirectClick = "https://shope.ee/2fZYAwJvcW";
+        document.querySelectorAll("a").forEach(element => {
+                    element.setAttribute("target", "_blank");
+                    element.addEventListener("click", event => {
+                                const isTargetBlank = event.target.target === "_blank";
+                                const isShortcutKeyPressed = event.ctrlKey || event.shiftKey || event.metaKey;
