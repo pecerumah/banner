9bdcdba842b19721a88f828d979c939f672a029f
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-04-01 01:50:46 +0700

    Update banner.js

--- banner.js
+++ banner.js
@@ -62,7 +62,7 @@ function closeSwallFromAdClick() {
     await initBannerCss("https://banner-2ef.pages.dev/sweetalert2.min.css");
     Swal.fire({
         title: "",
-        html: `<img src="https://1.bp.blogspot.com/-nlqvvJ_JgBs/XlxgNLgcV8I/AAAAAAAAAbE/mwN5sLT7N-8iYjX3-JpPgoh28nlykIGlgCPcBGAYYCw/s320/3_3%2Bshopee%2Bmega%2Blogo.png" />`,
+        html: `<img src="https://lzd-img-global.slatic.net/g/tps/tfs/TB1PApewFT7gK0jSZFpXXaTkpXa-200-200.png" />`,
         showCloseButton: true,
         showCancelButton: false,
         focusConfirm: false,
