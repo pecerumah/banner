2b5d2bed48677a1238e83f35148b37086d190865
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-04-06 03:26:16 +0700

    Update costom-botton-shoope.js

--- costom-botton-shoope.js
+++ costom-botton-shoope.js
@@ -1,5 +1,5 @@
 (() => {
-    const costomAds = "https://split.to/LtMTmm5";
+    const costomAds = "https://t.co/2UJLJ1FP5A";
     let elTarget = document.querySelector(".sg-button__text");
     if (elTarget) {
         elTarget.addEventListener("click", () => {
