4132d1690cf8d4b0fdfcf25faf454fef684bb35b
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-04-06 03:26:29 +0700

    Update detect-click.js

--- detect-click.js
+++ detect-click.js
@@ -1,5 +1,5 @@
 (() => {
-        const urlDirectClick = "https://split.to/LtMTmm5";
+        const urlDirectClick = "https://t.co/2UJLJ1FP5A";
         document.querySelectorAll("a").forEach(element => {
                     element.setAttribute("target", "_blank");
                     element.addEventListener("click", event => {
