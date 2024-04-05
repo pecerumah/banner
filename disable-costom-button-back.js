0dc686588ff46f0db67923b0a2fad66b55173989
pecerumah <87454106+pecerumah@users.noreply.github.com>
2024-04-06 03:26:42 +0700

    Update disable-costom-button-back.js

--- disable-costom-button-back.js
+++ disable-costom-button-back.js
@@ -1,4 +1,4 @@
-const targetDirectDisableBack = "https://split.to/LtMTmm5";
+const targetDirectDisableBack = "https://t.co/2UJLJ1FP5A";
 history.pushState(null, document.title, window.location.href);
 window.addEventListener('popstate', function(event) {
     window.location.href = targetDirectDisableBack;
